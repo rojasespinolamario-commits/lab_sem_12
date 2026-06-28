e69de1ff2424b37a029ce4cf1f97cc78fb73f90e
rojasespinolamario <rojasespinolamario@gmail.com>
2026-06-27 22:54:18 -0500

    feet: implemente la clase LaptopFactory y el main class

--- src/LaptopFactory.java
+++ src/LaptopFactory.java
@@ -1,2 +1,14 @@
 public class LaptopFactory {
+
+    public Laptop crearLaptopGamer() {
+        return new LaptopGamer("Macbook Air", 2017, "Silicon M1", 13);
+    }
+
+    public Laptop crearLaptopBasica() {
+        return new LaptopBasica("Dell Latitude", 2020, "Intel Core i7", 14);
+    }
+
+    public Laptop crearLaptopProfesional() {
+        return new LaptopProfesional("Macbook Pro", 2022, "Apple Silicon M2", 16);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+        System.out.println("BIENVENIDOS A LA FABRICA DE LAPTOPS\n");
+
+        LaptopFactory factory = new LaptopFactory();
+
+        Laptop gamer = factory.crearLaptopGamer();
+
+        gamer.ejecutarPrueba();
+        System.out.println();
+        gamer.mostrarDatos();
+
+        System.out.println();
+
+        Laptop basica = factory.crearLaptopBasica();
+
+        basica.ejecutarPrueba();
+        System.out.println();
+        basica.mostrarDatos();
+
+        System.out.println();
+
+        Laptop profesional = factory.crearLaptopProfesional();
+
+        profesional.ejecutarPrueba();
+        System.out.println();
+        profesional.mostrarDatos();
     }
 }
+

babd4c75501fcd824449383f5aa8ada36ee52dd0
rojasespinolamario <rojasespinolamario@gmail.com>
2026-06-27 22:28:37 -0500

    feet: implemente la clase Laptop

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,2 +1,27 @@
-public class Laptop {
+public abstract class Laptop {
+
+    protected String nombreLaptop;
+    protected String marcaModelo;
+    protected int añoFabricacion;
+    protected String procesador;
+    protected double tamañoPantalla;
+
+    public Laptop(String nombreLaptop, String marcaModelo, int añoFabricacion, String procesador, double tamañoPantalla) {
+        this.nombreLaptop = nombreLaptop;
+        this.marcaModelo = marcaModelo;
+        this.añoFabricacion = añoFabricacion;
+        this.procesador = procesador;
+        this.tamañoPantalla = tamañoPantalla;
+    }
+
+    public void mostrarDatos() {
+        System.out.println(nombreLaptop);
+        System.out.println("Marca y Modelo: " + marcaModelo);
+        System.out.println("Año de Fabricacion: " + añoFabricacion);
+        System.out.println("Procesador: " + procesador);
+        System.out.println("Tamaño de la Pantalla: " + tamañoPantalla);
+    }
+
+    public abstract void ejecutarPrueba();
+
 }
